ba96b2c2a8b83609b9e82d1424d612cbf65daf63
Rakesh <rakeshranjanray15@gmail.com>
2025-07-27 00:04:32 +0530

    updated login with product

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,124 +1,70 @@
-"use client";
+"use client"
+import React, { useState, useEffect } from 'react'
 
-import React, { useState } from "react";
-import {
-  Box,
-  Card,
-  CardContent,
-  TextField,
-  Button,
-  Typography,
-  InputAdornment,
-  IconButton,
-  Avatar,
-} from "@mui/material";
-import LockIcon from "@mui/icons-material/Lock";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
+function SearchInput() {
+  const [result, setresult] = useState(["Rakesh"])
+  const [uniqueCat, setuniqueCat] = useState([])
+  const [clickedcat, setclickedcat] = useState("")
+  const [renderdata, setrenderdata] = useState(null)
+  useEffect(() => {
+    const fetchdata = async () => {
 
-export default function LoginPage() {
-  const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+      try {
+        const res = await fetch("https://fakestoreapi.com/products")
+        const data = await res.json();
+        if (data) {
+          console.log(data)
+          setresult(data)
+          const cat = data.map(val => val.category)
+          console.log("cat", cat)
+          setuniqueCat(cat.filter((val, ind, self) => self.indexOf(val) === ind))
 
-  const handleTogglePassword = () => setShowPassword((prev) => !prev);
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchdata()
 
-  const handleLogin = () => {
-    alert(`Email: ${email}\nPassword: ${password}`);
-  };
+  }, [])
+  console.log("result", result)
 
   return (
-    <Box className="min-h-screen flex items-center justify-center bg-gradient-to-tr from-blue-100 to-purple-200 px-4">
-      <Card className="w-full max-w-md rounded-3xl shadow-2xl">
-        <CardContent className="p-8 sm:p-10">
-          <Box className="flex justify-center mb-6">
-            <Avatar className="bg-blue-600 w-16 h-16 shadow-md">
-              <LockIcon fontSize="large" />
-            </Avatar>
-          </Box>
+    <div className='p-10'>
+      <div className='flex justify-between'>
+        {uniqueCat && uniqueCat.map((val, ind) => {
+          return (
+            <button key={ind} className='border bg-red-200 p-1 text-sm cursor-pointer rounded' onClick={() => setclickedcat(val)}>
+              {val}
+            </button>
+          )
+        })}
+      </div>
+      <div className='flex justify-around   flex-wrap mt-5'>
+        {result && result.filter((val) => val.category === clickedcat).map((val, ind) => {
+          return (
+            <div key={ind} className='max-w-[300px] w-full border cursor-pointer rounded-xl flex flex-col justify-between my-3'>
+              <p className='bg-blue-200 w-fit rounded-xl m-2 p-1 '>
+                {val.category}
 
-          <Typography
-            variant="h5"
-            align="center"
-            className="font-bold mb-6 text-gray-800"
-          >
-            Sign in to your account
-          </Typography>
+              </p>
 
-          <Box className="flex flex-col gap-6">
-            <TextField
-              label="Email"
-              fullWidth
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              variant="outlined"
-              slotProps={{
-                input: {
-                  className: "py-3 px-2",
-                },
-                inputLabel: {
-                  shrink: true,
-                },
-              }}
-            />
+              <img src={val.image} className='max-w-[250px] max-h-[200px] object-contain' />
+              <p className='text-sm text-[#808080] p-3 font-bold'>
 
-            <TextField
-              label="Password"
-              fullWidth
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              variant="outlined"
-              slotProps={{
-                input: {
-                  className: "py-3 px-2",
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={handleTogglePassword} edge="end">
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                },
-                inputLabel: {
-                  shrink: true,
-                },
-              }}
-            />
+                $:{val.price}
+              </p>
 
-            <Box className="-mt-2 flex justify-end">
-              <Typography
-                variant="body2"
-                className="text-sm text-blue-600 hover:underline cursor-pointer"
-              >
-                Forgot password?
-              </Typography>
-            </Box>
+              <p className='text-sm text-justify p-3'>
+                {val.description}
 
-            <Button
-              variant="contained"
-              color="primary"
-              fullWidth
-              size="large"
-              className="rounded-full py-3 font-semibold"
-              onClick={handleLogin}
-            >
-              Login
-            </Button>
-
-            <Typography
-              variant="body2"
-              align="center"
-              className="text-gray-600 pt-2"
-            >
-              Don&apos;t have an account?{" "}
-              <span className="text-blue-600 font-medium hover:underline cursor-pointer">
-                Register
-              </span>
-            </Typography>
-          </Box>
-        </CardContent>
-      </Card>
-    </Box>
-  );
+              </p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
+
+export default SearchInput
